74797623c724cd3203cbdd4a1e8ce2aae3b45df4
ANASTASIOS BAIKAS <79874740+TasosBaikas@users.noreply.github.com>
2024-01-28 02:26:15 +0200

    finished

--- askisi_mvc_cinema/Controllers/ProvolesController.cs
+++ askisi_mvc_cinema/Controllers/ProvolesController.cs
@@ -14,13 +14,16 @@ namespace askisi_mvc_cinema.Controllers
     {
         ProvoliRepository provoliRepository;
         ReservationRepository reservationRepository;
-
-
+        MovieRepository movieRepository;
+        CinemaRepository cinemaRepository;
+        UserRepository userRepository;
         public ProvolesController()
         {
             provoliRepository = new ProvoliRepository();
             reservationRepository = new ReservationRepository();
-
+            movieRepository = new MovieRepository();
+            cinemaRepository = new CinemaRepository();
+            userRepository = new UserRepository();
         }
 
         [HttpGet]
@@ -62,7 +65,7 @@ namespace askisi_mvc_cinema.Controllers
                 return Json(result);
             }
 
-            ReservationModel reservationIfExist =  reservationRepository.GetReservationById(reservationModel.PROVOLES_ID,reservationModel.USER_USERNAME);
+            ReservationModel reservationIfExist = reservationRepository.GetReservationById(reservationModel.PROVOLES_ID, reservationModel.USER_USERNAME);
             if (reservationIfExist != null)
             {
                 result = new { success = false, message = "Έχετε κάνει ήδη κράτηση" };
@@ -75,7 +78,7 @@ namespace askisi_mvc_cinema.Controllers
             provoliRepository.UpdateProvoli(provoli);
 
 
-            result = new { success = true, message = "Επιτυχής κράτηση"};
+            result = new { success = true, message = "Επιτυχής κράτηση" };
             return Json(result);
         }
 
@@ -85,7 +88,7 @@ namespace askisi_mvc_cinema.Controllers
         public ActionResult History(HistoryViewModel historyViewModel)
         {
 
-            if (historyViewModel.USER_USERNAME == null)
+            if (historyViewModel.USER_USERNAME == null || historyViewModel.USER_USERNAME == "")
             {
                 return View();
             }
@@ -111,18 +114,128 @@ namespace askisi_mvc_cinema.Controllers
         [HttpGet]
         public ActionResult Create()
         {
+            ViewBag.Movies = movieRepository.GetAllMovies();
+            ViewBag.Cinemas = cinemaRepository.GetAllCinemas();
+            ViewBag.Users = userRepository.GetAllUsers();
             return View();
         }
 
-        [HttpGet]
-        public ActionResult Edit()
+        [HttpPost]
+        public ActionResult Create(ProvoliModel model)
         {
+            ViewBag.Movies = movieRepository.GetAllMovies();
+            ViewBag.Cinemas = cinemaRepository.GetAllCinemas();
+            ViewBag.Users = userRepository.GetAllUsers();
+
+            if (model.MOVIES_ID == null || model.MOVIES_ID == 0)
+            {
+                ViewBag.Message = "Να πάρετε ένα ταινία";
+                return View(model);
+            }
+
+            if (model.CINEMAS_ID == null || model.CINEMAS_ID == 0)
+            {
+                ViewBag.Message = "Να πάρετε ένα cinema";
+                return View(model);
+            }
+
+            if (model.NUMBER_OF_SEATS == null || model.NUMBER_OF_SEATS == 0)
+            {
+                ViewBag.Message = "Γράψτε πόσο θέσεις υπάρχουν";
+                return View(model);
+            }
+
+            model.NUMBER_OF_FREE_SEATS = model.NUMBER_OF_SEATS;
+
+            if (model.DATE_FROM == null)
+            {
+                ViewBag.Message = "Γράψτε date_from";
+                return View(model);
+            }
+
+            if (model.DATE_TO == null)
+            {
+                ViewBag.Message = "Γράψτε date_to";
+                return View(model);
+            }
+
+            if (model.USER_USERNAME == null)
+            {
+                ViewBag.Message = "Να πάρετε ένα user";
+                return View(model);
+            }
+
+            provoliRepository.AddProvoli(model);
+
+            ViewBag.message = "New provoli successfully created!";
+
             return View();
         }
 
+
         [HttpGet]
-        public ActionResult Reserve()
+        public ActionResult Edit(int? id)
         {
+            if (id != null)
+            {
+                ProvoliModel model = provoliRepository.GetProvoliById((int)id);
+                return View(model);
+            }
+            else
+            {
+                ViewBag.Message = "Δεν υπάρχει αυτή η προβολή. Πρέπει να κάνετε η νέα.";
+                return RedirectToAction("Create", "Provoles");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ProvoliModel model)
+        {
+            ViewBag.Movies = movieRepository.GetAllMovies();
+            ViewBag.Cinemas = cinemaRepository.GetAllCinemas();
+            ViewBag.Users = userRepository.GetAllUsers();
+
+            if (model.MOVIES_ID == null || model.MOVIES_ID == 0)
+            {
+                ViewBag.Message = "Να πάρετε ένα ταινία";
+                return View(model);
+            }
+
+            if (model.CINEMAS_ID == null || model.CINEMAS_ID == 0)
+            {
+                ViewBag.Message = "Να πάρετε ένα cinema";
+                return View(model);
+            }
+
+            if (model.NUMBER_OF_SEATS == null || model.NUMBER_OF_SEATS == 0)
+            {
+                ViewBag.Message = "Γράψτε πόσο θέσεις υπάρχουν";
+                return View(model);
+            }
+
+            model.NUMBER_OF_FREE_SEATS = model.NUMBER_OF_SEATS;
+
+            if (model.DATE_FROM == null)
+            {
+                ViewBag.Message = "Γράψτε date_from";
+                return View(model);
+            }
+
+            if (model.DATE_TO == null)
+            {
+                ViewBag.Message = "Γράψτε date_to";
+                return View(model);
+            }
+
+            if (model.USER_USERNAME == null)
+            {
+                ViewBag.Message = "Να πάρετε ένα user";
+                return View(model);
+            }
+
+            provoliRepository.UpdateProvoli(model);
+            ViewBag.Message = "Το προβολή άλλαξε!";
             return View();
         }
     }
---
+++ askisi_mvc_cinema/Controllers/RegisterClientController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+using askisi_mvc_cinema.helpers;
+using askisi_mvc_cinema.Models;
+using askisi_mvc_cinema.Repositories;
+using askisi_mvc_cinema.Services;
+
+namespace askisi_mvc_cinema.Controllers
+{
+    public class RegisterClientController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(UserModel model)
+        {
+
+            if (model.EMAIL == null || model.EMAIL.IsEmpty())
+            {
+                ViewBag.Message = "Γράψτε email";
+                return View();
+            }
+
+            if (model.USERNAME == null || model.USERNAME.IsEmpty())
+            {
+                ViewBag.Message = "Γράψτε username";
+                return View();
+            }
+
+            if (model.PASSWORD == null || model.PASSWORD.IsEmpty())
+            {
+                ViewBag.Message = "Γράψτε password";
+                return View();
+            }
+
+            UserRepository userRepository = new UserRepository();
+
+            UserModel modelFromDb = userRepository.GetUserByUsername(model.USERNAME);
+            if (modelFromDb != null)
+            {
+                ViewBag.Message = "User exists";
+                return View();
+            }
+
+            model.ROLE = UserRoles.Costumer;
+            model.CREATE_TIME = DateTime.Now;
+            model.SALT = GenerateRandomString.Generate(4);
+            model.PASSWORD = AuthenticateUser.HashPassword(model.PASSWORD, model.SALT);
+
+            userRepository.AddUser(model);
+
+            ViewBag.Message = "Success! customer registered!";
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
